ee39261627663e6dba64c8c7fbf0b7837b34656f
banu005 <banumathey5@gmail.com>
2025-03-28 12:08:04 +0100

    ajout formulaire

--- app/src/main/java/com/example/myapplication/93mooveScreen.kt
+++ app/src/main/java/com/example/myapplication/93mooveScreen.kt
@@ -1,5 +1,6 @@
 package com.example.myapplication
 
+import android.graphics.Color.blue
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
@@ -11,9 +12,15 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -21,6 +28,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -73,9 +81,40 @@ fun ListeCours(modifier: Modifier, navController: NavHostController) {
 
 }
 
-fun FormulaireLogin(modifier: Modifier, navController: NavHostController) {
+@Composable
+fun FormulaireLogin(modifier: Modifier, navController: NavController) {
+    var login by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+//
+//    val backgroundColor = if {
+//        login.length >= 10 -> Color.Red
+//        else -> Color.White
+//    }
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Card() {
+            TextField(
+                label = { Text("Entrez votre login") },
+                value = login,
+                onValueChange = { login = it },
+//              colors = TextFieldDefaults.textFieldColors(containerColor = backgroundColor),
+            )
+        }
+            Card() {
+                TextField(
+                    label = { Text("Entrez votre mot de passe") },
+                    value = password,
+                    onValueChange = { password = it }
+                )
+            }
+        }
+    }
 
-}
+
+
+
 
 @Composable
     fun PageAccueil(
@@ -88,19 +127,23 @@ fun FormulaireLogin(modifier: Modifier, navController: NavHostController) {
 
         val image = painterResource(R.drawable.logo)
         Column(
-            verticalArrangement = Arrangement.Center,
-        )
-        {
+//            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ){
             Image(
                 painter = image,
                 contentDescription = "logo 93Moove",
-                modifier = imageModifier
+                modifier = imageModifier,
             )
             Text(
-                text = "93 Moove " +
-                        "Bienvenue",
+                text = "93 Moove ",
                 fontSize = 36.sp,
-                lineHeight = 116.sp,
+                lineHeight = 50.sp,
+            )
+            Text(
+                text = "Bienvenue",
+                fontSize = 16.sp,
+                lineHeight = 36.sp,
             )
             Button(onClick = {navController.navigate(mooveScreen.Login.name){
                 popUpTo(mooveScreen.Start.name) { inclusive = false }
@@ -111,10 +154,6 @@ fun FormulaireLogin(modifier: Modifier, navController: NavHostController) {
             Button(onClick = {navController.navigate(mooveScreen.Login.name)}) {
                 Text("Se connecter")
             }
-            TextField(
-                value = "",
-                onValueChange = {}
-            )
         }
     }
 
